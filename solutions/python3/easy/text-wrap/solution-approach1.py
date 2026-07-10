c64bbd89ddbd29094ab695b5a17031aa5fae9942
Ujjwal Mohan <ujjwalmohan.ds@gmail.com>
2026-07-10 11:00:33 +0530

    ✅ Text Wrap [python3] — approach1

---
+++ solutions/python3/easy/text-wrap/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Problem     Text Wrap
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-10, 11:00 a.m.
+# ──────────────────────────────────────────────────
+
+
+
+def wrap(string, max_width):
+    fill_text = textwrap.fill(string , max_width)
+    
+    return fill_text
+    
+
